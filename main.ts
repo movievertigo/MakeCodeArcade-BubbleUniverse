dc0c78ae56d653b6e355905633d9c874d87076c4
Movie Vertigo <47259194+movievertigo@users.noreply.github.com>
2025-05-19 20:20:53 +0100

    Added movement and scale controls

--- main.ts
+++ main.ts
@@ -11,13 +11,31 @@ function Render () {
             ang2Offset = ang2Start + y
             x = Math.sin(ang1Offset) + Math.sin(ang2Offset)
             y = Math.cos(ang1Offset) + Math.cos(ang2Offset)
-            scene.backgroundImage().setPixel(xMid + x * size, yMid + y * size, colIdx)
+            scene.backgroundImage().setPixel(xMid + xOffset + x * size, yMid + yOffset + y * size, colIdx)
             colIdx += colInc
         }
         ang1Start += ang1Inc
         ang2Start += ang2Inc
     }
 }
+function controls (deltaTime: number) {
+    scale = 1 + deltaTime / 4000
+    if (controller.A.isPressed()) {
+        size = size * scale
+        xOffset = xOffset * scale
+        yOffset = yOffset * scale
+    }
+    if (controller.B.isPressed()) {
+        size = size / scale
+        xOffset = xOffset / scale
+        yOffset = yOffset / scale
+    }
+    xOffset += controller.dx(deltaTime * -2)
+    yOffset += controller.dy(deltaTime * -2)
+}
+let scale = 0
+let yOffset = 0
+let xOffset = 0
 let ang2Offset = 0
 let ang1Offset = 0
 let colIdx = 0
@@ -44,7 +62,10 @@ colInc = 3 / iterations
 xMid = scene.screenWidth() / 2
 yMid = scene.screenHeight() / 2
 size = scene.screenHeight() / 4
+let oldTime = game.runtime()
 game.onUpdate(function () {
     animTime = game.runtime() / 10000
     Render()
+    controls(game.runtime() - oldTime)
+    oldTime = game.runtime()
 })
